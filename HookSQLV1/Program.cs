06d200fc60800fcf5ac9789a46221180c0c0d850
shiu <kahshiu@gmail.com>
2016-03-20 14:49:18 +0800

    shiu initial commit from local

---
+++ HookSQLV1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace NotForRepoPreCommitHook
+{
+    class Program
+    {
+        const string NotForRepoMarker = "NOT_FOR_REPO";
+
+        static void Main(string[] args)
+        {
+            //string[] affectedPaths = File.ReadAllLines();
+
+            Console.Error.WriteLine("killer");
+            Environment.Exit(1);
+
+        }
+
+        static bool ContainsNotForRepoMarker(string path)
+        {
+            StreamReader reader = File.OpenText(path);
+
+            try
+            {
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    if (line.Contains(NotForRepoMarker))
+                    {
+                        return true;
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+            finally
+            {
+                reader.Close();
+            }
+
+            return false;
+        }
+    }
+}
